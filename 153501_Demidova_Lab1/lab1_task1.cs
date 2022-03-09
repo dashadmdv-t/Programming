477286a460620cfb18afdcec29e430c737ddcc24
dashadmdv <69718734+dashadmdv@users.noreply.github.com>
2022-03-09 21:42:19 +0300

    Add files via upload

---
+++ 153501_Demidova_Lab1/lab1_task1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _153501_Demidova
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int res = a / b;
+            Console.WriteLine(res);
+        }
+    }
+}
