ee8411b9a309a792b328fcbf65d42f1afd301ea2
dashadmdv <apushkins7@gmail.com>
2022-04-28 02:49:52 +0300

    lab4

---
+++ lab4/task1/Task1.cs
@@ -0,0 +1,105 @@
+﻿// вариант 4
+
+using System;
+
+namespace task1
+{
+    public class Airport
+    {
+        private static Airport instance;
+        private Airport()
+        { }
+        public static Airport GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Airport();
+            }
+            return instance;
+        }
+
+        private string name;
+        private Ticket ticket = new Ticket();
+        private int places;
+        private int sold;
+
+        public int Cash()
+        {
+            return sold * ticket.GetCost();
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+        public int GetPlaces()
+        {
+            return places;
+        }
+        public int GetSold()
+        {
+            return sold;
+        }
+
+        public void SetName(string n)
+        {
+            name = n;
+        }
+        public void SetPlaces(int n)
+        {
+            places = n;
+        }
+        public void SetSold(int n)
+        {
+            sold = n;
+        }
+
+        public void SetCost(int n)
+        {
+            ticket.SetCost(n);
+        }
+    }
+
+    public class Ticket
+    {
+        public Ticket()
+        {
+
+        }
+        public Ticket(int n)
+        {
+            SetCost(n);
+            FirstCost = n;
+        }
+
+        private static int cost;
+        private static int FirstCost = 0;
+
+        public void SetCost(int n)
+        {
+            cost = n;
+            if (FirstCost == 0)
+            {
+                FirstCost = n;
+            }
+        }
+
+        public void SetCost()
+        {
+            cost = FirstCost;
+        }
+
+        public int GetCost()
+        {
+            return cost;
+        }
+    }
+
+    class Task1
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
