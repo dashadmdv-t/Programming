23bfdb948de12471d7056443ee77e92db5f95fd0
dashadmdv-t <112268853+dashadmdv-t@users.noreply.github.com>
2022-08-28 22:15:24 +0300

    add line one more time (#2)

--- lab3/task1/Task1.cs
+++ lab3/task1/Task1.cs
@@ -12,6 +12,8 @@ namespace task1
         static void Main(string[] args)
         {
             Console.WriteLine(Func.F_n(2) + Func.F_n(3) + Func.F_n(4));
+
+	    Console.WriteLine("I CAN USE GODDAMN GIT!!!!");
         }
     }
 }
